f9e97ced599342e50b51dcd54346e8373b184fc0
Kopal100 <praveen23p@gmail.com>
2020-04-29 18:48:09 +0530

    Random changes c26

--- sketch.js
+++ sketch.js
@@ -19,14 +19,14 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
+    box1 = new Box(600,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
+    pig3 = new Pig(820, 220);
 
     log3 =  new Log(810,180,300, PI/2);
 
